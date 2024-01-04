c1bf9ed48c6bda8f3e8fd68a8ac1c5a22748eb5e
MrMinemeet <alex@wtf-my-code.works>
2024-01-04 15:20:26 +0100

    Course fetch from KUSSS Calendar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,16 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-	val name = "Kotlin"
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-	// to see how IntelliJ IDEA suggests fixing it.
-	println("Hello, " + name + "!")
+import java.net.URL
+import org.example.data.Student
 
-	for (i in 1..5) {
-		//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-		println("i = $i")
-	}
+fun main(args: Array<String>) {
+	// ===================== EXAMPLE USAGE OF KUSSS =====================
+	// Get URL From Args
+	val kusssCalendarURL = URL(args[1])
+
+	// Create Student from URL
+	val student = Student(args[0], kusssCalendarURL)
+	println(student)
+
+	// ==================================================================
 }
