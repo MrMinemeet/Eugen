7c86febaf9c51284d8c7f4f91d817557cc78bd71
MrMinemeet <alex@wtf-my-code.works>
2024-01-04 15:17:20 +0100

    Some adjustments of Constructors
    Adjust secondary constructor and add default values to primary one

--- src/main/kotlin/data/Student.kt
+++ src/main/kotlin/data/Student.kt
@@ -1,17 +1,27 @@
 package org.example.data
 
+import java.net.URL
+import org.example.Kusss
+import org.example.Util
+
 /**
  * Represents a student
  * @param discordName The discord name of the student
- * @param calendarToken The calendar token of the student
- * @param courses The courses of the student
- * @param studentId The student id of the student (if set by the user otherwise -1)
+ * @param userToken The calendar token of the student
+ * @param courses The courses of the student (fetched from KUSSS if not set)
+ * @param studentId The student id of the student (-1 if not set)
  */
 data class Student(
 	val discordName: String,
-	val calendarToken: String,
-	val courses: List<Course>,
-	val studentId: Int,
+	val userToken: String,
+	val courses: List<Course> = Kusss.getCourses(userToken),
+	val studentId: Int = -1,
 ) {
-	constructor(discordName: String, calendarToken: String, courses: List<Course>): this(discordName, calendarToken, courses, -1)
+	/**
+	 * Creates a new student with the given discord name and calendar token
+	 * @param discordName The discord name of the student
+	 * @param calendarURL The calendar url of the student
+	 */
+	constructor(discordName: String, calendarURL: URL) :
+			this(discordName, Util.tokenFromURL(calendarURL).orElse(""))
 }
