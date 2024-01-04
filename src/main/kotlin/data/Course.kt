cdd8ea00cee49063fc8f76eb0bcac43e034c2b6e
MrMinemeet <alex@wtf-my-code.works>
2024-01-04 10:00:56 +0100

    Add enum for LVA Type

--- src/main/kotlin/data/Course.kt
+++ src/main/kotlin/data/Course.kt
@@ -4,6 +4,7 @@ import java.net.URL
 
 /**
  * Represents a course itself
+ * @param lvaType The lva type of the course
  * @param lvaNr The lva number of the course
  * @param semester The semester of the course
  * @param lvaName The name of the course
@@ -11,6 +12,7 @@ import java.net.URL
  * @param url The url of the course
  */
 data class Course(
+	val lvaType: LvaType,
 	val lvaNr: UInt,
 	val semester: Semester,
 	val lvaName: String,
