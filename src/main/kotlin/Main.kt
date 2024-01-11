a8285456e85bcb03666972a80bbf8eddc32819a2
Bernhard Schertler <bernhard.schertler@pm.me>
2024-01-11 01:03:32 +0100

    Fix errors from merge

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 package org.example
 
-import java.net.URL
+import createTable
+import insertCourse
 import org.example.data.Student
+import java.net.URI
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val kusssCalendarURL = URL(args[1])
+fun main(args: Array<String>) {
+    val kusssCalendarURL = URI.create(args[1]).toURL()
 
     // Create Student from URL
     val student = Student(args[0], kusssCalendarURL)
