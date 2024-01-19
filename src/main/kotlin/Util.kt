4a86e940feffb1ffb7bf321ebc194898fbec0350
MrMinemeet <alex@wtf-my-code.works>
2024-01-19 12:17:00 +0100

    Add Caching to requests
    Uses a TTL of 4h
    
    Makes sense as many students will have the same courses

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -5,19 +5,50 @@ import java.net.URI
 import java.net.URL
 import java.util.*
 import javax.net.ssl.HttpsURLConnection
+import kotlin.concurrent.thread
+import kotlin.time.Duration.Companion.hours
+
 
 object Util {
+	private val CACHE_TTL: Long = 4.hours.inWholeMilliseconds // 4h of cache time
+	private val requestCache: MutableMap<URI, CachedResponse> = mutableMapOf()
+
 	/**
-	 * Reads the content of a URL as a string
-	 * @param url The URL to read from
-	 * @return The content of the URL as a string
-	 * @throws IOException If an error occurs while reading from the URL
+	 * Reads the content of a URI as a string
+	 * Utilizes caching with a defined [CACHE_TTL]
+	 * @param uri The URI to read from
+	 * @return The content of the URI as a string
 	 */
-	fun readTextFromURL(url: URI): String {
+	fun readTextFromURL(uri: URI): String {
+		if (requestCache.containsKey(uri) &&
+			requestCache.getValue(uri).timestamp + CACHE_TTL > System.currentTimeMillis()
+		) {
+			// Cache was valid -> reuse response
+			return requestCache.getValue(uri).response
+		}
+
+		// Shrink cache to remove old entries
+		thread(start = true, isDaemon = true, name = "Cache Shrinking Thread") {
+			shrinkCache()
+		}
+
+		// Create new Request, cache it and return response
+		val response = makeRequest(uri)
+		requestCache[uri] = CachedResponse(response, System.currentTimeMillis())
+		return response
+	}
+
+	/**
+	 * Reads the content of a URI as a string. Doesn't utilize caching
+	 * @param uri The URI to read from
+	 * @return The content of the URI as a string
+	 * @see readTextFromURL
+	 */
+	private fun makeRequest(uri: URI): String {
 		val response = StringBuilder()
 		// Read until null is returned// Get ical file from URL
 		try {
-			val huc = url.toURL().openConnection() as HttpsURLConnection
+			val huc = uri.toURL().openConnection() as HttpsURLConnection
 			val br = BufferedReader(InputStreamReader(huc.inputStream))
 
 			// Read until null is returned
@@ -33,6 +64,14 @@ object Util {
 	}
 
 	/**
+	 * Removes all entries from the cache that are older than [CACHE_TTL]
+	 */
+	private fun shrinkCache() {
+		val now = System.currentTimeMillis()
+		requestCache.entries.removeIf { it.value.timestamp + CACHE_TTL < now }
+	}
+
+	/**
 	 * Extracts the user token from a KUSSS Calendar URL
 	 * @param url The URL to extract the token from
 	 * @return The user token from the URL
@@ -43,4 +82,11 @@ object Util {
 		val group = matchResult.groups[1] ?: return Optional.empty()
 		return Optional.of(group.value)
 	}
+
+	/**
+	 * Cache entry for a request
+	 * @param response The response of the request
+	 * @param timestamp The timestamp of the request
+	 */
+	private data class CachedResponse(val response: String, val timestamp: Long)
 }
